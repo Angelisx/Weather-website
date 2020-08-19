3e0399c83ca99ccc4b7164ceb223e10729a912df
Angelisx <angeljsmythe@gmail.com>
2020-08-19 19:16:27 -0400

    Added the ability to input zipcodes

--- script.js
+++ script.js
@@ -1,28 +1,37 @@
-"use strict"
+
+$(document).ready(function () {
+
+
+    $('#button').click(function () {
+        let zipcode = $('#zip').val();
 
 
 
-const url = "https://api.openweathermap.org/data/2.5/weather?zip=11235,us&appid=ad56f041d195cd2846790c71286da726";
+
+        const url = "https://api.openweathermap.org/data/2.5/weather?zip=" + zipcode + ",us&appid=ad56f041d195cd2846790c71286da726";
 
 
-$.ajax({
-    url: url,
-    success: function (result) {
-        console.log(result);
-        console.log(result.name);
-        let f = Math.round(result.main.temp * (9 / 5) - 459.67);
-        let fahrenheit = f.toString();
+        $.ajax({
+            url: url,
+            success: function (result) {
+                console.log(result);
+                console.log(result.name);
+                let f = Math.round(result.main.temp * (9 / 5) - 459.67);
+                let fahrenheit = f.toString();
 
-        let w = Math.round(result.wind.speed / .44704);
-        let Windspeed = w.toString();
+                let w = Math.round(result.wind.speed / .44704);
+                let Windspeed = w.toString();
 
-        $('#weather').html(result.weather[0].description)
-        $('#location').html(result.name);
-        $('#temp').html(fahrenheit + "°");
-        $('#wind').html(Windspeed + " mph")
+                $('#weather').html(result.weather[0].description)
+                $('#location').html(result.name);
+                $('#temp').html(fahrenheit + "°");
+                $('#wind').html(Windspeed + " mph")
 
-       
 
-    }
-})
 
+            }
+        })
+
+    });
+
+});
